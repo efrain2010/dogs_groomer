01281f75a7600d9d1cdda812f671cc9544753216
efrain2010 <efrain.villanueva3@gmail.com>
2021-03-22 20:52:58 +0000

    Add global styled components

---
+++ src/global-styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const CtaButton = styled.button`
+  background-color:  transparent;
+  border: 1px solid #FFFFFF;
+  border-radius: 5px;
+  color: #FFFFFF;
+  cursor: pointer;
+  padding: 0.5rem 0.9rem;
+  position: relative;
+  top: -0.5rem;
+`;
+
+export {
+  CtaButton
+}
