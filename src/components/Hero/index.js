55f9fad68a523e1322308fc4d81209ce23640e2d
efrain2010 <efrain.villanueva3@gmail.com>
2021-03-22 21:26:58 +0000

    Update component to add styled component

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { StyledContainer, StyledFigure } from './styles';
+
 import heroImg from '../../assets/cachorro-tomando-una-ducha-2.jpg';
 
-import styles from './styles.module.css';
 
 const Hero = () => {
   return (
-    <div className={styles.hero}>
-      <figure>
+    <StyledContainer>
+      <StyledFigure>
         <img src={heroImg} alt="cachorro tomando una ducha" />
-        <div className="screen"></div>
-      </figure>
-    </div>
+      </StyledFigure>
+      <div>
+        <h2>Dándole a a tu mascota el mejor trato por que sabemos que es un miembro más de la familia</h2>
+      </div>
+    </StyledContainer>
   )
 }
 
