5abf8685c2145cba4f13c8df9777fd08e335e468
efrain2010 <efrain.villanueva3@gmail.com>
2021-03-23 17:33:38 +0000

    Add info and styles to Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
+import { StyledFooter, StyledFigure, StyledAddressText, StyledInfoBox, DesignedBy } from './styles';
+
+import logo from '../../assets/logo.png';
+
 const Footer = () => {
   return (
-    <footer>
-      This is a footer
-    </footer>
+    <StyledFooter>
+      <StyledFigure>
+        <img src={logo} alt="CALUDOB logotipo" />
+        <h2>CALUDOB</h2>
+        <h3>Dog Spa & Walks</h3>
+
+        <StyledAddressText><a target="_blank" rel="noopener noreferrer" href="https://goo.gl/maps/B9rSx3Tw9496bbKr8">Javier Barros Sierra #444<br/> Chetumal, Q. Roo</a></StyledAddressText>
+
+        <StyledInfoBox>
+          <a href="tel:9871375114">Teléfono<br/>987 137 5114</a>
+        </StyledInfoBox>
+      </StyledFigure>
+      <DesignedBy>
+        <h4><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/efrain-villanueva/">Diseñado y desarrollado por Efraín Villanueva</a></h4>
+      </DesignedBy>
+    </StyledFooter>
   )
 }
 
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+  background-color: var(--primary-color);
+  color: #FFFFFF;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-top: 2rem;
+
+  * {
+    margin: 0;
+    text-align: center;
+    font-size: 1.1rem;
+    font-weight: 500;
+  }
+
+  `;
+
+const StyledFigure = styled.figure`
+  img {
+    max-width: 6rem;
+  }
+`;
+
+const StyledAddressText = styled.h2`
+  margin-top: 1.5rem;
+
+  a {
+    color: #FFFFFF;
+  }
+`;
+
+const StyledInfoBox = styled.div`
+  margin-top: 1.5rem;
+
+  a {
+    color: #FFFFFF;
+    text-decoration: none;
+  }
+`;
+
+const DesignedBy = styled.div`
+  background-color: black;
+  margin-top: 2rem;
+  padding: 0.6rem 0;
+
+  h4 {
+    a {
+      color: #FFFFFF;
+      font-size: 0.8rem;
+    }
+
+    a:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+export {
+  StyledFooter,
+  StyledAddressText,
+  StyledFigure,
+  StyledInfoBox,
+  DesignedBy
+}
