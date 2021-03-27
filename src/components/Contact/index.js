c6f50ea474d31e6927f3809902968abb595e6389
efrain2010 <efrain.villanueva3@gmail.com>
2021-03-27 06:52:11 +0000

    Add Contact component

---
+++ src/components/Contact/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { StyledContainer } from './styles';
+
+const Contact = () => {
+  return (
+    <StyledContainer>
+      <h2>Contact</h2>
+    </StyledContainer>
+  )
+}
+
+export default Contact;
