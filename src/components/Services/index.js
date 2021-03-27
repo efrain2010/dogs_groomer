e6b7f889758d505e0a4ea73fc389addd177c93ac
efrain2010 <efrain.villanueva3@gmail.com>
2021-03-27 06:46:07 +0000

    Add services info

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,33 +1,57 @@
 import React from 'react';
 
-import { StyledContainer } from './styles';
+import { StyledContainer, ServiceContainer, StyledLogoService, StyledH2 } from './styles';
+
+import service1 from '../../assets/service-1.png';
+import service2 from '../../assets/service-2.png';
+import service3 from '../../assets/service-3.png';
 
 const Services = () => {
   return (
     <StyledContainer>
       <div className="grid">
-        <div className="col-6">
-          <h2>BAÑO SIMPLE</h2>
-        </div>
-        <div className="col-6">
-          <h2>BAÑO ESPECIAL<br/>(PARA QUITAR GARRAPATAS Y PULGAS)</h2>
-        </div>
         <div className="col-12">
-          <h3>LOS BAÑOS INCLUYEN:</h3>
-          <ul> 
-            <li>Limpieza corporal</li>
-            <li>Corte y/o limado de uñas</li>
-            <li>Limpieza de orejas</li>
-            <li>Lavado de dientes</li>
-            <li>Glandulas anales</li>
-          </ul>
+          <StyledH2>Nuestros Servicios</StyledH2>
         </div>
-        <div className="col-12">
-          <h2>Paseo de Mascotas</h2>
-        </div>
-        <div className="col-12">
-          <h2>Contamos con servicio a domicilio</h2>
-        </div>
+        <ServiceContainer>
+          <div className="col-sm-4">
+            <StyledLogoService>
+              <img src={service1} alt="Baño simple para mascotas"/>
+              <h2>Baño Simple</h2>
+              <ul> 
+                <li>Limpieza corporal</li>
+                <li>Corte y/o limado de uñas</li>
+                <li>Limpieza de orejas</li>
+                <li>Lavado de dientes</li>
+                <li>Glandulas anales</li>
+              </ul>
+            </StyledLogoService>
+          </div>
+          <div className="col-sm-4">
+            <StyledLogoService>
+              <img src={service2} alt="Baño especial para mascotas"/>
+              <h2>Baño Especial</h2>
+              <h3>Especial para quitar garrapatas y pulgas</h3>
+              <ul> 
+                <li>Limpieza corporal</li>
+                <li>Corte y/o limado de uñas</li>
+                <li>Limpieza de orejas</li>
+                <li>Lavado de dientes</li>
+                <li>Glandulas anales</li>
+              </ul>
+            </StyledLogoService>
+          </div>
+          <div className="col-sm-4">
+            <StyledLogoService>
+              <img src={service3} alt="Paseo de mascotas"/>
+              <h2>Paseo de Mascotas</h2>
+              <p>El perfecto desestres para tu mascota, ¿sabías que la mayoría de los perros rompe cosas en casa por estrés? Además de ayudar a ejercitar a tu mascota el salir a caminar ayuda a liberar ese estrés.</p>
+            </StyledLogoService>
+          </div>
+          <div className="col-sm-12">
+            <h3>Contamos con servicio a domicilio</h3>
+          </div>
+        </ServiceContainer>
       </div>
     </StyledContainer>
   );
--- src/components/Services/styles.js
+++ src/components/Services/styles.js
@@ -1,8 +1,48 @@
 import styled from 'styled-components';
 
 const StyledContainer = styled.div`
+  padding-bottom: 4rem;
+  text-align: center;
+
+  ul {
+    list-style: none;
+  }
+`;
+
+const ServiceContainer = styled.div`
+  padding: 0 2rem;
+`;
+
+const StyledLogoService = styled.div`
+  img {
+    max-width: 130px;
+    margin-bottom: 1rem;
+  }
+
+  h2 {
+    margin: 0;
+  }
+  
+  h3 {
+    font-size: 1rem;
+    line-height: 1rem;
+    margin: 0;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+    margin-top: 1rem;
+  }
+`;
+
+const StyledH2 = styled.h2`
+  font-size: 2rem;
 `;
 
 export {
-  StyledContainer
+  StyledContainer,
+  ServiceContainer,
+  StyledLogoService,
+  StyledH2
 }
